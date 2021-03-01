7477f1c32d403677a42cefef2c1fe6a40adf2a10
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-03-02 00:01:06 +0600

    Add project files.

---
+++ Lab_Task_Mid/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_Mid
+{
+    class Account
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
